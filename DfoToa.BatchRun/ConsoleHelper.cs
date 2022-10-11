6c9810a47499707b2d9c9d4b9493030b7c646fc1
Øivind Skovborg <oskov@usn.no>
2022-10-11 13:30:50 +0200

    Improved program startup

---
+++ DfoToa.BatchRun/ConsoleHelper.cs
@@ -0,0 +1,32 @@
+﻿namespace DfoToa.BatchRun
+{
+    internal class ConsoleHelper
+    {
+        private readonly bool _silent;
+        public ConsoleHelper(bool silent)
+        {
+            _silent = silent;
+        }
+
+        internal string? GetFromDate()
+        {
+            if (_silent) return null;
+            Console.Write("Angi startdato (yyyymmdd): ");
+            return Console.ReadLine();
+        }
+
+        internal string? GetToDate()
+        {
+            if (_silent) return null;
+            Console.Write("Angi sluttdato (yyyymmdd): ");
+            return Console.ReadLine();
+        }
+
+        internal bool Proceed(string message)
+        {
+            if (_silent) return true;
+            Console.Write(message);
+            return Console.ReadLine()?.ToLower()?.Contains("j") == true;
+        }
+    }
+}
--- DfoToa.BatchRun/Program.cs
+++ DfoToa.BatchRun/Program.cs
@@ -6,29 +6,29 @@ using System.Globalization;
 String? dateFrom = null;
 String? dateTo = null;
 
-#if DEBUG
+#if RELEASE
 dateFrom = "20220910";
 dateTo = "20220911";
 Boolean proceed = true;
 #else
+var argumentUtility = new ArgumentUtility(Environment.GetCommandLineArgs());
+if (argumentUtility.HelpNeeded()) return;
 
-if (Environment.GetCommandLineArgs().Length > 1) dateFrom = Environment.GetCommandLineArgs()[1];
-if (Environment.GetCommandLineArgs().Length > 2) dateTo = Environment.GetCommandLineArgs()[2];
-else if (dateFrom != null) dateTo = DateTimeOffset.Now.Date.ToString("yyyyMMdd");
+var consoleHelper = new ConsoleHelper(argumentUtility.Silent);
+
+dateFrom = argumentUtility.FromDate;
+dateTo = argumentUtility.ToDate;
 if (dateFrom == null)
 {
-    Console.Write("Angi startdato (yyyymmdd): ");
-    dateFrom = Console.ReadLine();
+    dateFrom = consoleHelper.GetFromDate();
 }
 if (dateTo == null)
 {
-    Console.Write("Angi sluttdato (yyyymmdd): ");
-    dateTo = Console.ReadLine();
+    dateTo = consoleHelper.GetToDate();
 }
-Console.WriteLine("Er du sikker på at du vil arkivere avtaler fra " + dateFrom + " til " + dateTo + "? Skriv ja for å fortsette eller trykk enter for å avslutte.");
 DefaultContext.FromDate = dateFrom;
 DefaultContext.ToDate = dateTo;
-Boolean proceed = Console.ReadLine() == "ja" ? true : false;
+bool proceed = consoleHelper.Proceed("Er du sikker på at du vil arkivere avtaler fra " + dateFrom + " til " + dateTo + "? (Ja/Nei) ");
 #endif
 
 
@@ -44,8 +44,7 @@ if (proceed)
             var handler = new ArchiveHandler(context);
             var contracts = await handler.GetContractsFromDfo(DateTimeOffset.ParseExact(dateFrom, "yyyyMMdd", CultureInfo.InvariantCulture),
                 DateTimeOffset.ParseExact(dateTo, "yyyyMMdd", CultureInfo.InvariantCulture));
-            Console.Write($"Fant {contracts.Count()} avtaler. Ønsker du å arkivere disse? (Ja/Nei) ");
-            if (Console.ReadLine()?.ToLower()?.Contains("n") == true) return;
+            if (!consoleHelper.Proceed($"Fant {contracts.Count()} avtaler. Ønsker du å arkivere disse? (Ja/Nei) ")) return;
             await handler.Archive(new P360EmployeeContractHandler(context), contracts);
         }
         catch (Exception ex)
